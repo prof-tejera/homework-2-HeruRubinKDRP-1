ed7c0d2449e813f211a7fcca3c14855e14762534
HeruRubinKDRP <herurubin@gmail.com>
2023-09-26 10:34:23 -0400

    support for github deployment

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>Hello CSCI E-39!</h1>
+        <p>Student: Heru Rubin</p>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
